e3118999aaf890376995438c99466794be3115f8
MSI <hirun08072002@gmail.com>
2024-11-08 14:51:10 +0530

    add for loop

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -29,6 +29,14 @@ public class BlackJack {
     public void buildDeck(){
         deck = new ArrayList<Card>();
         String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        String[] types = {"C","D","H","S"};
+
+
+        for(int i=0; i<types.length; i++){
+            for(int j=0; j<values.length; j++){
+                Card card= new Card(values[j], types[i])
+            }
+        }
 
     }
 
