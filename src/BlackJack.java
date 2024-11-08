c2e9afc901292c4b13efda0653214cbf742e3aa2
MSI <hirun08072002@gmail.com>
2024-11-08 14:35:58 +0530

    new file

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -6,12 +6,22 @@ import javax.swing.*;
 
 
 public class BlackJack {
+    private class Card {
+    String value;
+    String type;
+
+    Card(String value,String type){
+        this.value=value;
+        this.type= type;
+    }
+        
+    }
     BlackJack(){
         startGame();
     }
 
     public void startGame(){
-        
+
     }
 
 }
