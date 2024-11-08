a04943f21baf46f39082a0145ee6589dba9537b4
MSI <hirun08072002@gmail.com>
2024-11-08 14:57:39 +0530

    tostring

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -14,6 +14,10 @@ public class BlackJack {
             this.type = type;
         }
 
+        public String toString(){
+            return value + "-" + type;
+        }
+
     }
 
     ArrayList<Card> deck;
